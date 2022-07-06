ca37da6a03eb452ece6711380384211ce60f0996
Rammiloh <107976028+Rammiloh@users.noreply.github.com>
2022-07-06 13:22:05 +1000

    Update Generator.js

--- Generator.js
+++ Generator.js
@@ -1,1 +1,21 @@
 
+
+
+async function getJSON(jsonFile) {
+return fetch(jsonFile)
+	.then((response)=>response.json())
+	.then((responseJson)=>{return responseJson});
+}
+async function caller(jsonFile) {
+  var json = await this.getJSON();  // command waits until completion
+  console.log(json[0].Filename);            // hello is now available
+  return json;
+}
+
+var charactersJSON = caller("./Characters.json");
+var skillsJSON = caller("./Skills.json");
+var statsJSON = caller("./Classes.json");
+var conditionsJSON = caller("./WeaponConditions.json");
+var effectsJSON = caller("./WeaponEffects.json");
+
+document.getElementById("generatedUnit").innerHTML = `  <div id="charImage"> <img src="./CharacterIcons/${json[0].Filename}" /> </div>`;
